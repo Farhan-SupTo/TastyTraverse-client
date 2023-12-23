caa0c2d774c05f74b57f84e787ab539c1c8980b7
Your Name~ <you@example.com>
2023-12-23 22:30:25 +0600

    use tanstack query to add food item in a database and show them number of adding item on navbar while user login

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProviders from "./Providers/AuthProviders";
 import {
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './routes/router.jsx';
-import { HelmetProvider } from 'react-helmet-async';
-import AuthProviders from './Providers/AuthProviders';
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<AuthProviders>
-<HelmetProvider>
-<div className='max-w-screen-xl mx-auto '>
-    <RouterProvider router={router} />
-    </div>
-</HelmetProvider>
-</AuthProviders>
-  </React.StrictMode>,
-)
+    <AuthProviders>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <div className="max-w-screen-xl mx-auto ">
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
+      </HelmetProvider>
+    </AuthProviders>
+  </React.StrictMode>
+);
